abd91e948b858632fe3480de9c43f8dfb55983af
liyu18164127 <77355716+liyu18164127@users.noreply.github.com>
2021-04-13 05:14:43 +0800

    summary3

---
+++ lab3/iter_helpers.py
@@ -0,0 +1,22 @@
+def transpose(iterables):
+    import itertools
+
+    return itertools.zip_longest(*iterables)
+
+def scalar_product(scalar1, scalar2):
+    try:
+        return    sum(list(map(lambda e, f: int(str(e),0) * int(str(f),0), scalar1, scalar2)))
+    except ValueError:
+        return None
+
+if __name__ == '__main__':
+    #lab03 -task1
+    expected = [[1, 2], [-1, 3]]
+    actual = transpose([[1, -1], [2, 3]])
+    assert expected == list(map(list, actual))
+    #lab03 - task2
+    expected = 1
+    actual = scalar_product([1, '2'], [-1, 1])
+    assert expected == actual
+    actual = scalar_product([1, 'xyz'], [-1, 1])
+    assert actual is None
